23a75e97248420bb9b258ec23f3e71462bcf62bf
Sadjad Refagat <sadjadrefagat@gmail.com>
2025-08-04 19:54:05 +0330

    ---

--- file1.py
+++ file1.py
@@ -1,8 +1,11 @@
 a = int(input("Enter a natural number: "))
-
+"""_summary_
+"""
 for i in range(2, a):
     if a % i == 0:
         print(f"{a} is not a prime number.")
         break
 else:
     print(f"{a} is prime number.")
+    
+#sadjad refagat
